7b0e1b12dfa582d4752a9bf7460d3c5c529e2997
sandocleir2t-lang <sandocleir2t@gmail.com>
2026-08-06 12:44:18 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const express = require('express');
 const fs = require('fs');
 const EfiPay = require('sdk-node-apis-efi');
 const app = express();
+const cors = require('cors');
+app.use(cors({ origin: "*" }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  if(req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
 app.use(express.json()); app.use(express.urlencoded({extended:true}));
 
 const PLANOS = {
