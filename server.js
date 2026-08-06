5ca1f2c25e5f8fb50517370a656efa3d36497659
sandocleir2t-lang <sandocleir2t@gmail.com>
2026-08-06 18:23:12 -0300

    Add path module import to server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
+const path = require('path');
 const EfiPay = require('sdk-node-apis-efi');
 const app = express();
 app.use(cors());
